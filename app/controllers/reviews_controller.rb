7417689f1d6fbee751fc39234e77aa9bfdc64b3f
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-16 13:50:34 -0800

    started review stuff

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def index
+    @review = Review.new
+    @reviews = Review.all
+    respond_to do |format|
+      format.json
+      format.html
+    end
+  end
+
+  def show
+    @review = Review.find_by(id: params[:id])
+    render json: @review
+  end
+
+  def create
+    @review = Review.new(review_params)
+    unless @review.save
+      render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+    end
+    respond_to :js
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:title, :review_text, :reviewer_name, :movie_id)
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  before_create :set_review_date
+
+  private
+   def set_review_date
+     self.review_date = Date.today
+   end
 end
